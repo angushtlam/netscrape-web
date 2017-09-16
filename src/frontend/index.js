07d567a5cc6648396093a67b256e5308fb632f2a
Angus Lam <angushtlam@gmail.com>
2017-09-16 03:21:33 -0400

    Set up frontend and middleware.

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,2 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+class App extends React.Component {
+  render() {
+    return <h1>Hello World</h1>
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
